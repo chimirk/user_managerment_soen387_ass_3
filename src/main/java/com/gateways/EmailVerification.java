1d2ad78746cf517d60a25be9cbd99e711a7f3498
chimirk <chirca.mircea@gmail.com>
2021-11-27 19:43:06 -0500

    email verification link displayed properly email gateway pattern plugin pattern user gateway for saving data separated interface working emailGateway Gateway pattern working emailGateway plugin pattern working

--- src/main/java/com/gateways/EmailVerification.java
+++ src/main/java/com/gateways/EmailVerification.java
@@ -4,7 +4,7 @@ import javax.mail.MessagingException;
 import java.util.UUID;
 
 public interface EmailVerification {
-    public static final EmailVerification INSTANCE = (EmailVerification) PluginFactory.getPlugin(EmailVerification.class);
+    EmailVerification INSTANCE = (EmailVerification) PluginFactory.getPlugin(EmailVerification.class);
     void sendVerificationByEmail(String userEmail, UUID token) throws MessagingException;
 
 }
--- src/main/java/com/gateways/emailGateway.java
+++ src/main/java/com/gateways/emailGateway.java
@@ -1,27 +1,14 @@
 package com.gateways;
 
 import javax.mail.MessagingException;
+import java.util.Objects;
 import java.util.UUID;
 
 public class emailGateway {
 
-    //private EmailVerification emailVerification;
-
-    //public emailGateway(EmailVerification emailVerification) {
-        //this.emailVerification = emailVerification;
-    //}
-
-    /*public static void sendVerification(String userEmail, UUID token, EmailVerification emailVerification) throws MessagingException {
-
-        EmailVerification emailVerification = new EmailVerificationImpl();
-        emailVerification.sendVerificationByEmail(userEmail, token);
-
-    }*/
-
     public static void sendVerification(String userEmail, UUID token) throws MessagingException {
 
-        EmailVerification emailVerification = new EmailVerificationImpl();
-        emailVerification.sendVerificationByEmail(userEmail, token);
+        Objects.requireNonNull(EmailVerification.INSTANCE).sendVerificationByEmail(userEmail, token);
 
     }
 }
